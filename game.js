600211ada510d6b17e9c419006567d6c22d21ca5
ANGIL <informrawansa@gmail.com>
2026-09-07 15:23:52 +0300

    Initialize game with start message and HTML layout

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
+console.log("Mim 3D Game started! 🌸");
 
+document.body.innerHTML = `
+  <div style="
+    width:100vw;
+    height:100vh;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    background:#f8e8f0;
+    font-family:Arial;
+  ">
+    <h1>Mim's 3D World 🌸</h1>
+  </div>
+`;
